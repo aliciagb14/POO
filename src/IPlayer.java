0b3e872441a6345f33954fba85d6d450b0dbdc48
unknown <aliciagonzalezb14@gmail.com>
2022-12-30 23:52:40 +0100

    borrada version ant

--- src/IPlayer.java
+++
@@ -1,9 +0,0 @@
-public interface IPlayer {
-    void play(Connect4 game, int opcion);
-    void putToken(int column);
-    int getColumn();
-    int getRow();
-    Color getColor();
-    void writeWinner(Turn turn);
-    void setColor(Color color);
-}
--- src/MachinePlayer.java
+++
@@ -1,7 +0,0 @@
-public class MachinePlayer extends Player {
-
-    public MachinePlayer(Board board, Color color) {
-        super(board, color);
-    }
-    public void putTokenRandom(int column){ super.putToken(column); }
-}
